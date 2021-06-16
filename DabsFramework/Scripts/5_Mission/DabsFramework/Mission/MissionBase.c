905a70c68ce508b35b221c04e9a8607d466697a2
Tyler Paul <tylerpaul2329@gmail.com>
2021-06-16 13:40:11 -0400

    Enabled the EventManagert

---
+++ DabsFramework/Scripts/5_Mission/DabsFramework/Mission/MissionBase.c
@@ -0,0 +1,16 @@
+modded class MissionBase
+{
+	protected ref EventManager m_EventManager;
+	
+	override void OnInit()
+	{
+		super.OnInit();
+		
+		m_EventManager = new EventManager();
+	}
+	
+	EventManager GetEventManager()
+	{
+		return m_EventManager;
+	}
+}
