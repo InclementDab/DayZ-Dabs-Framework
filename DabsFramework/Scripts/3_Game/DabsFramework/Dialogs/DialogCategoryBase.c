53cd8b132a33f2ec252deff21442ca238b213684
Tyler Paul <tylerpaul2329@gmail.com>
2021-06-28 21:39:37 -0400

    Some of the starting logic is in yet

--- DabsFramework/Scripts/3_Game/DabsFramework/Dialogs/DialogCategoryBase.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/Dialogs/DialogCategoryBase.c
@@ -3,24 +3,27 @@ ref DialogCategoryBase m_DialogCategoryBase;
 void RunDialogTest()
 {
 	m_DialogCategoryBase = new DialogCategoryBase("Test");
-	m_DialogCategoryBase.AddContent("Testing", new SliderPrefab("Slider", null, ""));
-	m_DialogCategoryBase.AddContent("Testing", new SliderPrefab("Slider", null, ""));
+	
+	GroupPrefab group1 = new GroupPrefab("Group1", null, string.Empty);
+	group1.Insert(new SliderPrefab("Slider1", null, ""));
+	m_DialogCategoryBase.AddContent("Testing1", group1);
+	
+	GroupPrefab group2 = new GroupPrefab("Group2", null, string.Empty);
+	group2.Insert(new SliderPrefab("Slider2", null, ""));
+	m_DialogCategoryBase.AddContent("Testing2", group2);
+	
 	DialogResult result = m_DialogCategoryBase.ShowDialog();
-	Print(result);
 }
 
 
 class DialogCategoryBaseController: DialogBaseController
 {
-	ref map<ScriptView, ScriptView> DialogCategoryMap = new map<ScriptView, ScriptView>();
-	
 	ref ObservableCollection<ref DialogCategoryListItem> DialogCategoryData = new ObservableCollection<ref DialogCategoryListItem>(this);		
 
 	ScriptView CurrentContent;
 		
 	void ~DialogCategoryBaseController()
 	{
-		delete DialogCategoryMap;
 		delete DialogCategoryData;
 	}
 }
@@ -38,10 +41,16 @@ class DialogCategoryBase: DialogBase
 	{
 		content.SetParent(this);
 		
-		m_DialogCategoryBaseController.DialogCategoryData.Insert(new DialogCategoryListItem(category));
+		m_DialogCategoryBaseController.DialogCategoryData.Insert(new DialogCategoryListItem(category, content, this));
 		
-		m_DialogCategoryBaseController.DialogContentData.Insert(content);
+		//m_DialogCategoryBaseController.DialogContentData.Insert(content);
 		return content;
+	}
+	
+	void SetActiveCategory(DialogCategoryListItem list_item)
+	{
+		m_DialogCategoryBaseController.CurrentContent = list_item.GetContent();
+		m_DialogCategoryBaseController.NotifyPropertyChanged("CurrentContent");
 	}
 	
 	override typename GetControllerType() 
@@ -63,14 +72,25 @@ class DialogCategoryListItemController: ViewController
 
 class DialogCategoryListItem: ScriptViewTemplate<DialogCategoryListItemController>
 {
+	protected DialogCategoryBase m_DialogCategoryBase;
 	protected ScriptView m_Content;
 	
-	void DialogCategoryListItem(string caption, ScriptView content)
+	void DialogCategoryListItem(string caption, ScriptView content, DialogCategoryBase parent)
 	{
 		m_Content = content;
+		m_DialogCategoryBase = parent;
 		
 		m_TemplateController.Caption = caption;
 		m_TemplateController.NotifyPropertyChanged("Caption");
+	}
+		
+	void OnListItemExecute(ButtonCommandArgs args)
+	{
+		if (!m_DialogCategoryBase) {
+			return;
+		}
+		
+		m_DialogCategoryBase.SetActiveCategory(this);
 	}
 	
 	ScriptView GetContent()
