d554f17d9577729f7b77eb9ba851131cffde8a83
Tyler Paul <tylerpaul2329@gmail.com>
2021-06-30 16:30:16 -0400

    code cleanup

--- DabsFramework/Scripts/3_Game/DabsFramework/Dialogs/DialogCategoryBase.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/Dialogs/DialogCategoryBase.c
@@ -20,15 +20,8 @@ class DialogCategoryBase: DialogBase
 	void AddContent(string caption, array<ref ScriptView> content)
 	{
 		DialogCategoryListItem category_list_item = new DialogCategoryListItem(caption);
-		foreach (ScriptView script_view: content) {
-			category_list_item.AddContent(script_view);
-			script_view.SetParent(this);
-			script_view.GetLayoutRoot().Show(false);
-			m_DialogCategoryBaseController.DialogContentData.Insert(script_view);
-		}
-		
-		category_list_item.SetParent(this);
-		m_DialogCategoryBaseController.DialogCategoryData.Insert(category_list_item);
+		category_list_item.SetContent(content);
+		AddContent(category_list_item);
 	}
 	
 	void AddContent(DialogCategoryListItem category_list_item)
@@ -41,7 +34,6 @@ class DialogCategoryBase: DialogBase
 		}
 		
 		category_list_item.SetParent(this);
-		
 		m_DialogCategoryBaseController.DialogCategoryData.Insert(category_list_item);
 	}
 	
@@ -61,53 +53,5 @@ class DialogCategoryBase: DialogBase
 	override string GetLayoutFile()
 	{
 		return "DabsFramework/GUI/layouts/dialogs/DialogCategoryBase.layout";
-	}
-}
-
-class DialogCategoryListItemController: ViewController
-{
-	string Icon;
-	string Caption;
-}
-
-class DialogCategoryListItem: ScriptViewTemplate<DialogCategoryListItemController>
-{
-	protected DialogCategoryBase m_DialogCategoryBase;
-	
-	protected ref array<ref ScriptView> m_Content = {};
-	
-	void DialogCategoryListItem(string caption)
-	{
-		m_TemplateController.Caption = caption;
-		m_TemplateController.NotifyPropertyChanged("Caption");
-	}
-		
-	void OnListItemExecute(ButtonCommandArgs args)
-	{
-		if (!m_DialogCategoryBase) {
-			return;
-		}
-		
-		m_DialogCategoryBase.SetActiveCategory(this);
-	}
-	
-	void SetParent(DialogCategoryBase dialog)
-	{
-		m_DialogCategoryBase = dialog;
-	}
-	
-	void AddContent(ScriptView content)
-	{
-		m_Content.Insert(content);
-	}
-	
-	array<ref ScriptView> GetContent()
-	{
-		return m_Content;
-	}
-	
-	override string GetLayoutFile() 
-	{
-		return "DabsFramework/gui/Layouts/prefabs/ListItems/CategorizedListItem.layout";
 	}
 }
---
+++ DabsFramework/Scripts/3_Game/DabsFramework/Dialogs/DialogCategoryListItem.c
@@ -0,0 +1,52 @@
+class DialogCategoryListItemController: ViewController
+{
+	string Icon;
+	string Caption;
+}
+
+class DialogCategoryListItem: ScriptViewTemplate<DialogCategoryListItemController>
+{
+	protected DialogCategoryBase m_DialogCategoryBase;
+	
+	protected ref array<ref ScriptView> m_Content = {};
+	
+	void DialogCategoryListItem(string caption)
+	{
+		m_TemplateController.Caption = caption;
+		m_TemplateController.NotifyPropertyChanged("Caption");
+	}
+		
+	void OnListItemExecute(ButtonCommandArgs args)
+	{
+		if (!m_DialogCategoryBase) {
+			return;
+		}
+		
+		m_DialogCategoryBase.SetActiveCategory(this);
+	}
+	
+	void SetParent(DialogCategoryBase dialog)
+	{
+		m_DialogCategoryBase = dialog;
+	}
+	
+	void SetContent(array<ref ScriptView> content)
+	{
+		m_Content = content;
+	}
+	
+	void AddContent(ScriptView content)
+	{
+		m_Content.Insert(content);
+	}
+	
+	array<ref ScriptView> GetContent()
+	{
+		return m_Content;
+	}
+	
+	override string GetLayoutFile() 
+	{
+		return "DabsFramework/gui/Layouts/prefabs/ListItems/CategorizedListItem.layout";
+	}
+}
