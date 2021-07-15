733bd107e41feb699227c065a9a6d23c68b64a2e
Tyler Paul <tylerpaul2329@gmail.com>
2021-07-15 03:00:57 -0400

    Optimized EditorDeletedObjectData::FindObject

--- DabsFramework/Scripts/3_Game/DabsFramework/Editor/EditorDeletedObjectData.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/Editor/EditorDeletedObjectData.c
@@ -24,7 +24,7 @@ class EditorDeletedObjectData: SerializableBase
 		data.Position = position;
 		
 		// todo handle multiple objects of the same name in a 1m radius
-		data.WorldObject = data.FindObject(0.05);
+		data.WorldObject = data.FindObject();
 		return data;
 	}
 	
@@ -41,14 +41,15 @@ class EditorDeletedObjectData: SerializableBase
 		return data;
 	}
 	
-	Object FindObject(float radius = 0.05)
-	{
+	Object FindObject(float radius = 0.0001)
+	{				
 		array<Object> objects = {};
-		array<CargoBase> cargos = {};
-		GetGame().GetObjectsAtPosition3D(Position, radius, objects, cargos);
-		
+		GetGame().GetObjectsAtPosition3D(Position, radius, objects, null);
+				
 		foreach (Object object: objects) {
-			if (object.GetType() == Type) {
+			string type;
+			g_Game.ObjectGetType(object, type);
+			if (type == Type) {
 				return object;
 			}
 		}
