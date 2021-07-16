4e507371d4db1b020f5e3c8016074f3b62e67335
Tyler Paul <tylerpaul2329@gmail.com>
2021-07-16 13:06:14 -0400

    Added Dabbing to the gestures menu

---
+++ DabsFramework/Scripts/5_Mission/DabsFramework/GUI/GesturesMenu.c
@@ -0,0 +1,16 @@
+modded class GesturesMenu
+{
+	override void OnMenuRelease()
+	{
+		super.OnMenuRelease();
+	}
+
+	override void GetGestureItems(out ref array<ref GestureMenuItem> gesture_items, GestureCategories category)
+	{
+		super.GetGestureItems(gesture_items, category);
+		
+		if (category == GestureCategories.CATEGORY_3) {
+			gesture_items.Insert( new GestureMenuItem( EmoteConstants.ID_EMOTE_DABBING, 	"#STR_USRACT_ID_EMOTE_DABBING", 	GestureCategories.CATEGORY_2 ) );
+		}
+	}
+}
