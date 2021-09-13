8538554f3f075fefe4db0c4ee227c75c3ca05f78
Tyler Paul <tylerpaul2329@gmail.com>
2021-09-13 14:58:28 -0400

    Added TownFlags and parameters when enumerating towns

--- DabsFramework/Scripts/3_Game/DabsFramework/Town/Town.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/Town/Town.c
@@ -1,40 +1,18 @@
 class Town
 {
 	string Name;
-	string Type;
+	TownFlags Type;
 	vector Position;
 	
-	int GetSize()
-	{
-		switch (Type) {
-			case "Capital": return 500;
-			case "City": 	return 300;
-			case "Village": return 100;
-			
-			// This is for incorrectly configured maps
-			case "NameCapital": return 500;
-			case "NameCity": return 300;
-			case "NameVillage": return 100;
-		}
-		
-		return 0;
-	}
-	
-	float GetDistance()
-	{	
-		return vector.Distance(GetGame().GetPlayer().GetPosition(), Position);
-	}
-	
 	// default world_name parameter will load the active world
-	static array<ref Town> GetMapTowns(string world_name = "")
+	static array<ref Town> GetMapTowns(TownFlags flags = (TownFlags.CAPITAL | TownFlags.CITY), string world_name = "")
 	{
 		if (world_name == string.Empty) {
 			GetGame().GetWorldName(world_name);
 		}
 		
 		array<ref Town> town_positions = {};
-		string cfg = "CfgWorlds " + world_name + " Names";		
-		string allowed_types = "Capital City Village NameCapital NameCity NameVillage";
+		string cfg = "CfgWorlds " + world_name + " Names";
 		for (int i = 0; i < GetGame().ConfigGetChildrenCount(cfg); i++) {
 			string city;
 			GetGame().ConfigGetChildName(cfg, i, city);	
@@ -43,16 +21,78 @@ class Town
 			GetGame().ConfigGetFloatArray(string.Format("%1 %2 position", cfg, city), float_array);
 			city_position[0] = float_array[0]; city_position[2] = float_array[1];
 			city_position[1] = GetGame().SurfaceY(city_position[0], city_position[2]);
-				
-			if (allowed_types.Contains(GetGame().ConfigGetTextOut(string.Format("%1 %2 type", cfg, city)))) {
-				Town town_entry();
-				town_entry.Type = GetGame().ConfigGetTextOut(string.Format("%1 %2 type", cfg, city));
-				town_entry.Name = GetGame().ConfigGetTextOut(string.Format("%1 %2 name", cfg, city));
-				town_entry.Position = city_position;
-				town_positions.Insert(town_entry);
+			
+			TownFlags flag = GetTownFlag(GetGame().ConfigGetTextOut(string.Format("%1 %2 type", cfg, city)));
+			Print(typename.EnumToString(TownFlags, flag));
+			Print(flags & flag);
+			if ((flags & flag) == flag) {
+				Print("different castle");
+				continue;
 			}
+			
+			Town town_entry();
+			town_entry.Type = flag;
+			town_entry.Name = GetGame().ConfigGetTextOut(string.Format("%1 %2 name", cfg, city));
+			town_entry.Position = city_position;
+			town_positions.Insert(town_entry);
 		}
 		
 		return town_positions;
+	}
+	
+	static string GetTownTypeString(TownFlags flag)
+	{
+		switch (flag) {
+			case TownFlags.CAPITAL: return "Capital";
+			case TownFlags.CITY: 	return "City";
+			case TownFlags.VILLAGE: return "Village";
+			case TownFlags.CAMP: 	return "Camp";
+			case TownFlags.LOCAL: 	return "Local";
+			case TownFlags.HILL: 	return "Hill";
+			case TownFlags.MARINE: 	return "Marine";
+			case TownFlags.RUIN: 	return "Ruin";
+			case TownFlags.RAILROAD:return "RailroadStation";
+			case TownFlags.OFFICE: 	return "LocalOffice";
+			case TownFlags.VIEWPOINT:return "ViewPoint";
+		}
+		
+		return string.Empty;
+	}
+	
+	static int GetTownFlag(string town_type)
+	{
+		switch (town_type) {
+			case "Capital": return TownFlags.CAPITAL;
+			case "City": return TownFlags.CITY;
+			case "Village": return TownFlags.VILLAGE;
+			case "Camp": return TownFlags.CAMP;
+			case "Local": return TownFlags.LOCAL;
+			case "Hill": return TownFlags.HILL;
+			case "Marine": return TownFlags.MARINE;
+			case "Ruin": return TownFlags.RUIN;
+			case "RailroadStation": return TownFlags.RAILROAD;
+			case "LocalOffice": return TownFlags.OFFICE;
+			case "ViewPoint": return TownFlags.VIEWPOINT;
+		}
+		
+		return 0;
+	}
+	
+	int GetSize()
+	{
+		switch (Type) {
+			case TownFlags.CAPITAL: return 500;
+			case TownFlags.CITY: 	return 300;
+			case TownFlags.VILLAGE: return 100;
+			
+			// This is for incorrectly configured maps
+		}
+		
+		return 0;
+	}
+	
+	float GetDistance()
+	{	
+		return vector.Distance(GetGame().GetPlayer().GetPosition(), Position);
 	}
 }
