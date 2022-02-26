66f94574f3053cbe794a834072a602305a364a5b
Tyler Paul <tylerpaul2329@gmail.com>
2022-02-26 18:37:58 -0500

    Delete EventManagerUpdateParams.c

--- DabsFramework/Scripts/3_Game/DabsFramework/Types/EventManagerUpdateParams.c
+++
@@ -1,7 +0,0 @@
-// 0: Event Type
-// 1: Event Phase
-// 3: Event Phase Time Remaining
-// 4: Event Paused
-// 5: Event Data (type)
-// 6: Event Data (data)
-typedef Param6<string, int, float, bool, string, Serializer> EventManagerUpdateParams;
