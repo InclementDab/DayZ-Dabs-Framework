aff12ff04e8f27102e8147687bdfc3895e506817
Tyler Paul <tylerpaul2329@gmail.com>
2023-05-19 17:46:48 -0400

    Update EditorDeletedObjectData.c

--- DabsFramework/Scripts/3_Game/DabsFramework/Editor/EditorDeletedObjectData.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/Editor/EditorDeletedObjectData.c
@@ -73,7 +73,7 @@ class EditorDeletedObjectData: SerializableBase
 			}
 		}
 		
-		Error(string.Format("[DF] Failed to find %1 in %2m of %3", Type, radius, Position)); 
+		//Error(string.Format("[DF] Failed to find %1 in %2m of %3", Type, radius, Position)); 
 		return null;
 	}
 	
