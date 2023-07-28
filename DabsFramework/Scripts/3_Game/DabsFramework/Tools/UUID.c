66abf96871dbbbee65ca55b1c34eb952437fd9b7
InclementDab <tylerpaul2329@gmail.com>
2023-07-28 16:06:27 -0400

    Guid -> UUID
    Using DayZ vanilla standard. Packed into a simple class

---
+++ DabsFramework/Scripts/3_Game/DabsFramework/Tools/UUID.c
@@ -0,0 +1,11 @@
+typedef string UUID;
+class UUID: string
+{
+	static const UUID EMPTY = "00000000-0000-0000-0000-00000000";	
+	static UUID Generate()
+	{
+		int uid[4];
+		UUIDApi.Generate(uid);
+		return UUIDApi.FormatString(uid);
+	}
+}
