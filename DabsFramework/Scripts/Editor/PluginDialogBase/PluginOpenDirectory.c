24379e27cfae11575e088ddaa173851ef87bb2cc
Tyler Paul <tylerpaul2329@gmail.com>
2023-08-01 18:04:56 -0400

    Update PluginOpenDirectory.c

--- DabsFramework/Scripts/Editor/PluginDialogBase/PluginOpenDirectory.c
+++ DabsFramework/Scripts/Editor/PluginDialogBase/PluginOpenDirectory.c
@@ -7,6 +7,8 @@ class PluginOpenDirectory: PluginDialogBase
 		m_ScriptEditor.GetCurrentFile(current_file);
 		string absolute_directory = GetDirectory(GetAbsolutePath(current_file));		
 		absolute_directory.Replace(PATH_SEPERATOR, PATH_SEPERATOR_ALT);
+		
+		// rip unix bozos
 		Workbench.RunCmd(string.Format("explorer %1", absolute_directory));
 	}
 }
