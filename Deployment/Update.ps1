96b012882ad980c138fd83d335cea46056496be6
Tyler Paul <tylerpaul2329@gmail.com>
2023-11-03 19:00:28 -0400

    Update Update.ps1

--- Deployment/Update.ps1
+++ Deployment/Update.ps1
@@ -26,6 +26,35 @@ function Download-Tools {
     return $destination
 }
 
+function Get-RepositoryRoot {
+    return (Get-Item $PSScriptRoot).parent.FullName;
+}
+
+function Get-ModPrefix {
+    # Define the path to the .prefix file in the current directory
+    $prefix_file = Join-Path -Path (Get-RepositoryRoot) -ChildPath '.prefix'
+
+    # Check if the .prefix file exists
+    while (-Not (Test-Path $prefix_file)) {
+        $prefix = Read-Host -Prompt 'Enter your mod prefix'
+        if ("" -eq $prefix) {
+            Write-Error "Invalid prefix"
+            continue
+        }
+
+        $prefix_folder = Join-Path -Path (Get-RepositoryRoot) -ChildPath $prefix
+        if (-Not (Test-Path $prefix_folder)) {
+            Write-Error "The path specified is invalid ($prefix_folder)"
+        } else {
+            Set-Content -Path $prefix_file $prefix
+        }
+    }
+
+    # Return the contents of the .prefix file
+    return Get-Content $prefix_file
+}
+
+
 # Get current template version
 Write-Host "Fetching latest version of tools..."
 try {
@@ -38,6 +67,9 @@ try {
 Write-Host "Latest Version: '$latest_version'"
 
 [Version]$current_version = Get-ToolsVersion
+
+$current_directory = Join-Path -Path (Get-RepositoryRoot) -ChildPath (Get-ModPrefix)
+Write-Host "Current Directory: '$current_directory'"
 
 # Tools are up to date, no need to do anything
 if ($current_version -eq $latest_version) {
@@ -74,5 +106,22 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 # Delete zip file
 Remove-Item $download_archive -Force
 
+# Only update files in the ModTemplate that are .c and .cfg files (for now, todo?)
+$filtered_files = Get-ChildItem -Path "$download_folder\ModTemplate" -Recurse -File -Include *.c
 
+# Actually update the files
+foreach ($filtered_file in $filtered_files) {
+    # Replace 'ModTemplate' with our prefix in the file's directory path
+    $relative_path = $filtered_file.FullName.Substring("$download_folder\ModTemplate".Length)
 
+    $new_file = Join-Path -Path $current_directory -ChildPath $relative_path
+    $new_file_directory = Split-Path -Path $new_file
+
+    # Create the directory if it doesn't exist
+    if (-not (Test-Path $new_file_directory)) {
+        New-Item -Path $new_file_directory -ItemType Directory
+    }
+
+    # Copy the file to the new directory, overwriting if it already exists
+    Copy-Item -Path $filtered_file.FullName -Destination $new_file -Force
+}
