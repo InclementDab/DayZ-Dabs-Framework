86706b031df7de8bf20ac2ab70578a9324470e2e
Tyler Paul <tylerpaul2329@gmail.com>
2023-11-03 15:29:24 -0400

    Moved template

---
+++ Template/ModTemplate/Scripts/config.cpp
@@ -0,0 +1,74 @@
+
+class CfgPatches
+{
+	class ModTemplate_Scripts
+	{
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 0.1;
+		requiredAddons[] = {"DZ_Scripts"};
+	};
+};
+
+class CfgMods 
+{
+	class ModTemplate_Config
+	{
+		name = "Mod Template";
+		dir = "ModTemplate";
+		credits = "";
+		author = "";
+		creditsJson = "ModTemplate/Scripts/Data/Credits.json";
+		inputs = "ModTemplate/Scripts/Data/Inputs.xml";
+		type = "mod";
+		dependencies[] =
+		{
+			"Game", "World", "Mission"
+		};
+		class defs
+		{
+			class imageSets
+			{
+				files[]= {};
+			};
+			class engineScriptModule
+			{
+				value = "";
+				files[] =
+				{
+					"ModTemplate/scripts/common",
+					"ModTemplate/scripts/1_core"
+				};
+			};
+
+			class gameScriptModule
+			{
+				value="";
+				files[] = 
+				{
+					"ModTemplate/scripts/common",
+					"ModTemplate/scripts/3_Game"
+				};
+			};
+			class worldScriptModule
+			{
+				value="";
+				files[] = 
+				{
+					"ModTemplate/scripts/common",
+					"ModTemplate/scripts/4_World"
+				};
+			};
+
+			class missionScriptModule 
+			{
+				value="";
+				files[] = 
+				{
+					"ModTemplate/scripts/common",
+					"ModTemplate/scripts/5_Mission"
+				};
+			};
+		};
+	};
+};
