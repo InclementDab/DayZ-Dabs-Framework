fa2e96f37f48ba7369b21ac70b5370e133ea6a72
Tyler Paul <tylerpaul2329@gmail.com>
2023-11-03 19:21:38 -0400

    This was so much headache

--- DabsFramework/Workbench/MakeLink.ps1
+++ DabsFramework/Workbench/MakeLink.ps1
@@ -1,49 +1,4 @@
-function Get-RepositoryRoot {
-    return (Get-Item $PSScriptRoot).parent.parent.FullName;
-}
-
-function Get-ModPrefix {
-    return (Get-Item $PSScriptRoot).parent.Name;
-
-    # Define the path to the .prefix file in the current directory
-    $prefix_file = Join-Path -Path (Get-RepositoryRoot) -ChildPath '.prefix'
-
-    # Check if the .prefix file exists
-    while (-Not (Test-Path $prefix_file)) {
-        $prefix = Read-Host -Prompt 'Enter your mod prefix'
-        if ("" -eq $prefix) {
-            Write-Error "Invalid prefix"
-            continue
-        }
-
-        $prefix_folder = Join-Path -Path (Get-RepositoryRoot) -ChildPath $prefix
-        if (-Not (Test-Path $prefix_folder)) {
-            Write-Error "The path specified is invalid ($prefix_folder)"
-        } else {
-            Set-Content -Path $prefix_file $prefix
-        }
-    }
-
-    # Return the contents of the .prefix file
-    return Get-Content $prefix_file
-}
-
-function Get-Workdrive {
-    $workdrive = "P:\"
-
-    while (-Not (Test-Path -Path $workdrive)) {
-        # Get workdrive name
-        $workdrive = Read-Host -Prompt 'Destination Path (P:\)'
-        if ("" -eq $workdrive) {
-            $workdrive = "P:\"
-        }
-    }
-
-    return $workdrive
-}
-
-# Self elevating powershell script
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+. ".\include.ps1"
 
 $source = Join-Path -Path (Get-RepositoryRoot) -ChildPath (Get-ModPrefix)
 
@@ -62,7 +17,7 @@ if (Test-Path -Path $destination) {
 # Check if the destination directory already exists, if it does just do nothing
 try {
     # Create symbolic link
-    New-Item -ItemType SymbolicLink -Path $destination -Target $source
+    New-Item -ItemType Junction -Path $destination -Target $source
 } catch {
     Write-Error "Failed to create SymLink: $_"
 }
