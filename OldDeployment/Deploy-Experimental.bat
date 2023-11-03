61b97f07e87a80e9d3af53e89d295958da7285f9
Tyler Paul <tylerpaul2329@gmail.com>
2023-11-03 16:47:36 -0400

    moved

--- OldDeployment/Deploy-Experimental.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Deploying to Experimental. Are you sure?
-
-start /W "Build PBO" "%CD%/DabsFramework/Workbench/Batchfiles/ZBinarizeDeploy.bat"
-echo Uploading to Workshop
-cd "%CD%/DabsFramework/Workbench/Deployment"
-start /W "Upload to Workshop" Deployment.exe experimental
