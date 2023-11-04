1cf44f2aa639cc4806cdb230b51c4a9479b7d368
Tyler Paul <tylerpaul2329@gmail.com>
2023-11-04 02:37:18 -0400

    Update Init.ps1

--- Init.ps1
+++ Init.ps1
@@ -3,15 +3,34 @@
 # Get user input
 $name = Read-Host -Prompt 'Enter your mod name (DayZ-Mod-Name)'
 $name = $name.Replace(" ", "")
+$name = $name.Replace("\t", "")
+$name = $name.Replace("\n", "")
+if ("" -eq $name) {
+    $name = "DayZ-Mod-Name"
+}
 
 $prefix = Read-Host -Prompt 'Enter your mod prefix (ModName)'
 $prefix = $prefix.Replace(" ", "")
+$prefix = $prefix.Replace("\t", "")
+$prefix = $prefix.Replace("\n", "")
+if ("" -eq $prefix) {
+    $prefix = "ModName"
+}
 
 $destination = Read-Host -Prompt 'Enter destination directory (C:\Dev\proj)'
+if ("" -eq $destination) {
+    $destination = "C:\Dev\proj"
+}
 
 $target_directory = Join-Path -Path $destination -ChildPath $name
 
+# Check if this folder already exists
 if (Test-Path -Path $target_directory) {
+
+    # DEBUG!!! remove me please on releases
+    Remove-Item $target_directory -Force -Recurse
+    continue
+
     Write-Error "The folder already exists ($target_directory)"
     Read-Host "Press Enter to exit..."
     return
