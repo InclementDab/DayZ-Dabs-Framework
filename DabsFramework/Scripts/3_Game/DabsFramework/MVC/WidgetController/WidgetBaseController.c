47c54787bab40767fe2b1a14dfe9fa80d50d2171
Tyler Paul <tylerpaul2329@gmail.com>
2023-11-29 19:28:59 -0500

    Fixed unintended behavior when binding script views to widgets

--- DabsFramework/Scripts/3_Game/DabsFramework/MVC/WidgetController/WidgetBaseController.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/MVC/WidgetController/WidgetBaseController.c
@@ -2,20 +2,8 @@ class WidgetBaseController: WidgetControllerTemplate<Widget>
 {
 	override void Set(TypeConverter type_converter)
 	{
-		if (!m_Widget) {
-			return;
-		}
-
-		Widget parent = m_Widget.GetParent();
-		int sort = m_Widget.GetSort();
-		if (type_converter.GetWidget()) {
-			m_Widget = type_converter.GetWidget();
-
-			if (parent) {
-				parent.AddChild(m_Widget, false);
-			}
-
-			m_Widget.SetSort(sort);
+		if (m_Widget && type_converter.GetWidget()) {
+			m_Widget.AddChild(type_converter.GetWidget());
 		}
 	}
 
