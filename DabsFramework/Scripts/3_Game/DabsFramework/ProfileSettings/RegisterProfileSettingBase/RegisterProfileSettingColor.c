7cb95183ba2dcdc1995aa7cb041a45ed8766c3d2
Tyler Paul <tylerpaul2329@gmail.com>
2024-01-10 15:16:27 -0500

    Create RegisterProfileSettingColor.c

---
+++ DabsFramework/Scripts/3_Game/DabsFramework/ProfileSettings/RegisterProfileSettingBase/RegisterProfileSettingColor.c
@@ -0,0 +1,21 @@
+class RegisterProfileSettingColor: RegisterProfileSettingBase
+{
+	void RegisterProfileSettingColor(string category_name, string variable_name, string display_name, bool require_restart = false)
+	{
+		typename profile_settings_type = GetCurrentProfileSetting();
+		
+		// find existing category
+		array<ref ProfileSettingsCategory> categories = Instances[profile_settings_type];
+		foreach (ProfileSettingsCategory category: categories) {
+			if (category && category.Header == category_name) {
+				category.Settings.Insert(new ProfileSettingColor(variable_name, display_name, require_restart));
+				return; // we found it
+			}
+		}
+			
+		// create new category
+		ProfileSettingsCategory new_category = new ProfileSettingsCategory(category_name);
+		new_category.Settings.Insert(new ProfileSettingColor(variable_name, display_name, require_restart));
+		Instances[profile_settings_type].Insert(new_category);
+	}
+}
