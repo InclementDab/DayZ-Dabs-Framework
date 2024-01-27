89c0b6053a483120a14a05ea4a2044490b931ffb
Wardog <wardog@wrdg.net>
2024-01-27 05:13:03 -0600

    Increase useability of discord webhook types

--- DabsFramework/Scripts/3_Game/DabsFramework/Managed/Discord/DiscordPayload_EmbedField.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/Managed/Discord/DiscordPayload_EmbedField.c
@@ -6,6 +6,16 @@ class DiscordPayload_EmbedField: Managed
 	
 	void DiscordPayload_EmbedField(string _name, string _value, bool _inline = false)
 	{
+		if (_name == string.Empty)
+		{
+			ErrorEx("Name can not be empty!");
+		}
+		
+		if (_value == string.Empty)
+		{
+			ErrorEx("Value can not be empty!");
+		}
+		
 		name = _name;
 		value = _value;
 		inline = _inline;
--- DabsFramework/Scripts/3_Game/DabsFramework/Managed/Discord/DiscordPayload_Webhook.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/Managed/Discord/DiscordPayload_Webhook.c
@@ -3,10 +3,10 @@ class DiscordPayload_Webhook: Managed
 	string username;
 	string content;
 	string avatar_url;
-	ref array<ref DiscordPayload_Embed> embeds = {}; // max of 10
+	ref array<ref DiscordPayload_Embed> embeds = {};
 	string thread_name;
 	
-	void DiscordPayload_Webhook(string _username, string _content, string _avatar_url, array<ref DiscordPayload_Embed> _embeds = null, string _thread_name = string.Empty)
+	void DiscordPayload_Webhook(string _username = string.Empty, string _content = string.Empty, string _avatar_url = string.Empty, array<ref DiscordPayload_Embed> _embeds = null, string _thread_name = string.Empty)
 	{
 		username = _username;
 		content = _content;
@@ -16,5 +16,43 @@ class DiscordPayload_Webhook: Managed
 		}
 		
 		thread_name = _thread_name;
+	}
+
+	void SetUser(string _username, string _avatar_url = string.Empty)
+	{
+		username = _username;
+		avatar_url = _avatar_url;
+	}
+
+	void SetContent(string _content)
+	{
+		content = _content;
+	}
+
+	void SetThreadName(string _thread_name)
+	{
+		thread_name = _thread_name;
+	}
+
+	DiscordPayload_Embed CreateEmbed(string _title, string _description)
+	{
+		DiscordPayload_Embed embed = new DiscordPayload_Embed(_title, _description);
+		AddEmbed(embed);
+
+		return embed;
+	}
+
+	void AddEmbed(DiscordPayload_Embed embed)
+	{
+		if (!embeds) {
+			embeds = new array<ref DiscordPayload_Embed>();
+		}
+
+		if (embeds.Count() > 9) {
+			ErrorEx("Maximum number of embeds allowed is 10!");
+			return;
+		}
+
+		embeds.Insert(embed);
 	}
 }
