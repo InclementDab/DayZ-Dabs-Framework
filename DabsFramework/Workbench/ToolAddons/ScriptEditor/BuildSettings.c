5b4454e98507423a9360f27cfd16ded2d6f6c590
Tyler Paul <tylerpaul2329@gmail.com>
2024-02-01 14:36:56 -0500

    Build tool updates

--- DabsFramework/Workbench/ToolAddons/ScriptEditor/BuildSettings.c
+++ DabsFramework/Workbench/ToolAddons/ScriptEditor/BuildSettings.c
@@ -1,6 +1,6 @@
 class BuildSettings: SerializableBase
 {
-	static const int VERSION = 2;
+	static const int VERSION = 3;
 		
 	protected string m_CurrentFileLocation;
 	
@@ -12,6 +12,12 @@ class BuildSettings: SerializableBase
 	
 	[Attribute("", "combobox", "Copy Addons", "", ParamEnumArray.FromEnum(YesNo) )]
 	bool CopyAddons;
+		
+	[Attribute("", "editbox", "Private Key")]
+	string Key;
+	
+	[Attribute("", "editbox", "Extra Launch Arguments")]
+	string Args;
 	
 	void Save(string file)
 	{
@@ -47,6 +53,7 @@ class BuildSettings: SerializableBase
 		if (!FileExist(file)) {
 			//settings.BuildFolders; // default all masked
 			settings.Command = "pboProject";
+			settings.Args = "+H";
 			settings.Save(file);
 			return settings;
 		}
@@ -71,6 +78,8 @@ class BuildSettings: SerializableBase
 		serializer.Write(BuildFolders);
 		serializer.Write(Command);
 		serializer.Write(CopyAddons);
+		serializer.Write(Key);
+		serializer.Write(Args);
 	}
 	
 	override bool Read(Serializer serializer, int version)
@@ -92,6 +101,19 @@ class BuildSettings: SerializableBase
 		}
 		
 		if (!serializer.Read(CopyAddons)) {
+			return false;
+		}
+		
+		if (version <= 2) {
+			Args = "+H";
+			return true;
+		}
+		
+		if (!serializer.Read(Key)) {
+			return false;
+		}
+		
+		if (!serializer.Read(Args)) {
 			return false;
 		}
 		
