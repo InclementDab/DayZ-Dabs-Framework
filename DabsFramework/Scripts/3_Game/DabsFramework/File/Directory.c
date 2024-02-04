f2484c4731a3a502e758c4d5cf1af25d5d84dc33
Tyler Paul <tylerpaul2329@gmail.com>
2024-02-04 14:14:00 -0500

    Update Directory.c

--- DabsFramework/Scripts/3_Game/DabsFramework/File/Directory.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/File/Directory.c
@@ -1,35 +1,11 @@
 class Directory: FileSystem
-{
-	array<string> EnumerateFiles(string filter = "*")
-	{
-		array<string> enumerated_files = {};
-		string file_name;
-		FileAttr file_attributes;
-		FindFileHandle handle = FindFile(value + PATH_SEPERATOR + "*", file_name, file_attributes, FindFileFlags.DIRECTORIES);
-		if (!handle) {
-			return enumerated_files;
-		}
-		
-		if (File.WildcardMatch(file_name, filter)) {
-			enumerated_files.Insert(value + PATH_SEPERATOR + file_name);
-		}
-		
-		while (FindNextFile(handle, file_name, file_attributes)) {
-			if (File.WildcardMatch(file_name, filter)) {
-				enumerated_files.Insert(value + PATH_SEPERATOR + file_name);
-			}
-		}
-		
-		CloseFindFile(handle);
-		return enumerated_files;
-	}
-		
+{		
 	static array<string> EnumerateFiles(string directory, string filter = "*")
 	{
 		array<string> enumerated_files = {};
 		string file_name;
 		FileAttr file_attributes;
-		FindFileHandle handle = FindFile(directory + PATH_SEPERATOR + "*", file_name, file_attributes, FindFileFlags.DIRECTORIES);
+		FindFileHandle handle = FindFile(directory + PATH_SEPERATOR + "*", file_name, file_attributes, FindFileFlags.ALL);
 		if (!handle) {
 			return enumerated_files;
 		}
