3fb90f4ce71adfb1ac10c09962329ec244d8063b
Tyler Paul <tylerpaul2329@gmail.com>
2024-02-06 17:33:18 -0500

    Update NamedLocation.c

--- DabsFramework/Scripts/3_Game/DabsFramework/!Core/NamedLocation.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/!Core/NamedLocation.c
@@ -11,6 +11,11 @@ class NamedLocation: ConfigObject
 	static const string RAILROAD = "RailroadStation";
 	static const string OFFICE = "LocalOffice";
 	static const string VIEWPOINT = "ViewPoint";
+	static const string BIGMILITARY = "BigMilitary";
+	static const string MEDMILITARY = "MediumMilitary";
+	static const string SMALLMILITARY = "SmallMilitary";
+	static const string FEEDSHACK = "FeedShack";
+	static const string DEERSTAND = "DeerStand";
 	
 	[ConfigEntryAttribute("name")]
 	string Name;
@@ -20,6 +25,78 @@ class NamedLocation: ConfigObject
 	
 	[ConfigEntryAttribute("type")]
 	string Type;
+	
+	Symbols GetIcon()
+	{
+		switch (Type) {
+			case CAPITAL:
+			case CITY:
+			case VILLAGE: return string.Empty;
+			case CAMP: return Symbols.CAMPFIRE;
+			case LOCAL: return string.Empty;
+			case HILL: return Symbols.MOUNTAIN;
+			case MARINE: return Symbols.WAVE;
+			case RUIN: return Symbols.HILL_ROCKSLIDE;
+			case RAILROAD: return Symbols.TRAIN;
+			case OFFICE: return Symbols.BUILDING;
+			case VIEWPOINT: return Symbols.BINOCULARS;
+			case BIGMILITARY: return Symbols.PERSON_RIFLE;
+			case MEDMILITARY: return Symbols.SHIELD;
+			case SMALLMILITARY: return Symbols.KNIFE;
+			case FEEDSHACK: return Symbols.WHEAT;
+			case DEERSTAND: return Symbols.TOWER_OBSERVATION;
+		}
+		
+		return Symbols.CIRCLE_QUESTION;
+	}
+	
+	float GetImportance()
+	{
+		switch (Type) {
+			case CAPITAL: return 1.0;
+			case CITY: return 0.7;
+			case VILLAGE: return 0.7;
+			case CAMP: return 0.7;
+			case LOCAL: return 0.35;
+			case HILL: return 0.15;
+			case MARINE: return 0.5;
+			case RUIN: return 0.3;
+			case RAILROAD: return 0.3;
+			case OFFICE: return 0.25;
+			case VIEWPOINT: return 0.25;
+			case BIGMILITARY: return 0.65;
+			case MEDMILITARY: return 0.5;
+			case SMALLMILITARY: return 0.35;
+			case FEEDSHACK: return 0.15;
+			case DEERSTAND: return 0.1;
+		}
+		
+		return 1.0;
+	}
+	
+	Color GetColor()
+	{
+		switch (Type) {
+			case CAPITAL: return Color.RED;
+			case CITY: return Color.REDDIT;
+			case VILLAGE: return Color.BLUE;
+			case CAMP: return Color.ORANGE;
+			case LOCAL: return Color.DIM_GRAY;
+			case HILL: return Color.ORANGE;
+			case MARINE: return Color.SKY_BLUE;
+			case RUIN: return Color.BEIGE;
+			case RAILROAD: return Color.BROWN;
+			case OFFICE: return Color.LIGHT_GRAY;
+			case VIEWPOINT: return Color.GOLDENROD;
+			case BIGMILITARY: 
+			case MEDMILITARY: 
+			case SMALLMILITARY: return Color.DARK_ORCHID;
+			case FEEDSHACK: return Color.GOLD;
+			case DEERSTAND: return Color.BURLYWOOD;
+		}
+		
+		return 0;
+	}
 	
 	static void OnPositionUpdate(notnull NamedLocation instance)
 	{
