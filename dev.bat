d80f34c92cb9f5bdb45177e4cef0aaff7cc040e1
Tyler Paul <tylerpaul2329@gmail.com>
2024-02-07 22:06:50 -0500

    Update dev.bat

--- dev.bat
+++ dev.bat
@@ -2,10 +2,11 @@
 
 set cwd=%cd%
 
-cd DabsFramework
-
-taskkill /f /im "workbenchApp.exe"
+start /wait taskkill /f /im workbenchApp.exe
 
 for /f "tokens=2,*" %%a in ('reg query "HKCU\SOFTWARE\Bohemia Interactive\Dayz Tools" /v "path" 2^>nul') do set "dayz_tools=%%b"
+for /f "tokens=2,*" %%a in ('reg query "HKLM\SOFTWARE\WOW6432Node\Bohemia Interactive\Dayz exp" /v "main" 2^>nul') do set "dayz_dir=%%b"
 
-"%dayz_tools%\Bin\Workbench\workbenchApp.exe" "-mod=P:\DabsFramework;P:\JM\CF -profiles=P:\Profiles\Workbench -repository=%cwd%"
+cd /d "P:\"
+
+"%dayz_tools%\Bin\Workbench\workbenchApp.exe" "-mod=DabsFramework\Scripts" "-profiles=Profiles" "-repository=%cwd%" "-dayz=%dayz_dir%" -newErrorsAreWarnings=1
