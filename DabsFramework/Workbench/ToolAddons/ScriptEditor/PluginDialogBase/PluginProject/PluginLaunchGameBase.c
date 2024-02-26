400909e5bc4adc88fd357352e91d8b9318e49387
Tyler Paul <tylerpaul2329@gmail.com>
2024-02-25 19:41:24 -0500

    Update PluginLaunchGameBase.c

--- DabsFramework/Workbench/ToolAddons/ScriptEditor/PluginDialogBase/PluginProject/PluginLaunchGameBase.c
+++ DabsFramework/Workbench/ToolAddons/ScriptEditor/PluginDialogBase/PluginProject/PluginLaunchGameBase.c
@@ -155,7 +155,7 @@ class PluginLaunchGameBase: PluginProject
 		
 		string client_launch_params = LaunchSettings.BASE_LAUNCH_PARAMS + string.Format(" \"-mod=%1\" \"-profiles=%2\"", formatted_mod_list, client_profile_directory);
 		string server_launch_params = LaunchSettings.BASE_LAUNCH_PARAMS + string.Format(" \"-mod=%1\" \"-profiles=%2\" \"-serverMod=%3\" \"-config=%4\" \"-mission=%5\" -server", formatted_mod_list, server_profile_directory, formatted_server_mod_list, m_ServerConfig, server_mission);
-		string offline_launch_params = LaunchSettings.BASE_LAUNCH_PARAMS + string.Format(" \"-mod=%1\" \"-profiles=%2\" \"-mission=%3\"", formatted_mod_list, client_profile_directory, server_mission);
+		string offline_launch_params = LaunchSettings.BASE_LAUNCH_PARAMS + string.Format(" \"-mod=%1\" \"-profiles=%2\" \"-mission=%3\\Missions\\%4.%5\"", formatted_mod_list, client_profile_directory, launch_settings.Repository, mod_prefix, launch_settings.Map);
 
 		string ip, password;
 		int port;
