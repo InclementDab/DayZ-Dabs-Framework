a07b36296c78c57df3b2b1c977c963543422a94b
Tyler Paul <tylerpaul2329@gmail.com>
2024-04-12 16:22:10 -0400

    Added PlayerIdentity::FindByPlainId

--- DabsFramework/Scripts/3_Game/DabsFramework/!Core/PlayerIdentity.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/!Core/PlayerIdentity.c
@@ -13,4 +13,18 @@ modded class PlayerIdentity
 		
 		return null;
 	}
+	
+	// use PlayerIdentity::GetPlainId()
+	static PlayerIdentity FindByPlainId(string plain_id)
+	{
+		array<PlayerIdentity> player_identities = {};
+		GetDayZGame().GetPlayerIndentities(player_identities);
+		foreach (PlayerIdentity player_identity: player_identities) {
+			if (player_identity.GetPlainId() == plain_id) {
+				return player_identity;
+			}
+		}
+		
+		return null;
+	}
 }
