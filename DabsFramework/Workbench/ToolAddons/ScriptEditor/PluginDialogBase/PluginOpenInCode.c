f1417b6f43e3ffd2e3f217b5a3d28b21f949c625
Tyler Paul <tylerpaul2329@gmail.com>
2024-05-11 09:08:58 -0400

    Added PluginOpenInCode, use with Ctrl + =

--- DabsFramework/Workbench/ToolAddons/ScriptEditor/PluginDialogBase/PluginOpenInCode.c
+++ DabsFramework/Workbench/ToolAddons/ScriptEditor/PluginDialogBase/PluginOpenInCode.c
@@ -4,9 +4,13 @@ class PluginOpenInCode: PluginDialogBase
 	void PluginOpenInCode()
 	{
 		string current_file;
-		m_ScriptEditor.GetCurrentFile(current_file);		
-		Workbench.RunCmd(string.Format("code %1", GetAbsolutePath(current_file)));
+		m_ScriptEditor.GetCurrentFile(current_file);
+		string absolute_directory = GetAbsolutePath(current_file);
+		absolute_directory.Replace(PATH_SEPERATOR_ALT, PATH_SEPERATOR);
 		
-		Workbench.RunCmd("call sendkeys.bat \"code.exe\" \" \"");
+		int current_line = m_ScriptEditor.GetCurrentLine() + 1;
+		if (Workbench.RunCmd(string.Format("cmd /c \"code --goto %1:%2:1\"", absolute_directory, current_line))) {
+			ErrorDialog("Failed to open vscode");
+		}
 	}
 }
