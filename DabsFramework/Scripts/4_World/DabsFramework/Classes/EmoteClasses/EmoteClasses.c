167c06f2f23d51fdf162bdcbf581823ecdc2a99b
Tyler Paul <tylerpaul2329@gmail.com>
2024-06-25 12:16:05 -0400

    Fixed dabbing through walls

--- DabsFramework/Scripts/4_World/DabsFramework/Classes/EmoteClasses/EmoteClasses.c
+++ DabsFramework/Scripts/4_World/DabsFramework/Classes/EmoteClasses/EmoteClasses.c
@@ -8,4 +8,18 @@ modded class EmoteDabbing
 		m_AdditiveCallbackUID = DayZPlayerConstants.CMD_GESTUREMOD_DABBING;
 		m_HideItemInHands = true;
 	}
+	
+	override bool EmoteCondition(int stancemask)
+	{
+		vector head_pos;
+		MiscGameplayFunctions.GetHeadBonePos(m_Player, head_pos);
+		// checking for a wall in front
+		Ray ray = new Ray(head_pos, m_Player.GetDirection());
+		Raycast ray_cast = ray.PerformRaycastRV(m_Player, null, 0, 1.0);
+		if (ray_cast) {
+			return false;
+		}
+		
+		return super.EmoteCondition(stancemask);
+	}
 }
