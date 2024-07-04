e358f38ac2c9a377099eb631a812f2869d9cb0fd
Tyler Paul <tylerpaul2329@gmail.com>
2024-07-04 11:15:08 -0400

    some of this is outdated

--- DabsFramework/Scripts/1_Core/DabsFramework/Managed/Classes/RegisterMissionSetting.c
+++ DabsFramework/Scripts/1_Core/DabsFramework/Managed/Classes/RegisterMissionSetting.c
@@ -1,11 +1,3 @@
-class GenericWrapper: Managed
-{
-    typename GetDataType(int variable_index = 0)
-    {
-        return Type().GetVariableType(variable_index);
-    }
-}
-
 class RegisterMissionSetting: Class
 {
 	static ref map<typename, string> s_RegisteredInstances = new map<typename, string>();
