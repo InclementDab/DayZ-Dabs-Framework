e2f9de809086f05daed690dafd0fa622acfa7ba7
Tyler Paul <tylerpaul2329@gmail.com>
2025-07-14 17:36:24 -0400

    The classic method

--- DabsFramework/Scripts/3_Game/DabsFramework/Suppression/SuppressedObject.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/Suppression/SuppressedObject.c
@@ -20,7 +20,8 @@ class SuppressedObject: SerializableBase
 		object.GetTransform(transform);
 		copyarray(m_Transform, transform);
 		
-		object.SetScale(0);
+		object.SetScale(0.000001);
+		object.SetPosition(Vector(0, -10000, 0) + object.GetPosition());
 		
 		object.ClearFlags(m_Flags, true);
 		object.ClearEventMask(m_Events);
