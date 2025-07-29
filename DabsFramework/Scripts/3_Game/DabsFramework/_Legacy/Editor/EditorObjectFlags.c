5bd425978d67ed6610fdd00f5d21a0172be49b7f
Tyler Paul <tylerpaul2329@gmail.com>
2025-07-29 11:59:56 -0400

    Added EditorObjectFlag::BRUSHED

--- DabsFramework/Scripts/3_Game/DabsFramework/_Legacy/Editor/EditorObjectFlags.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/_Legacy/Editor/EditorObjectFlags.c
@@ -1,7 +1,7 @@
 const int EFE_DEFAULT = EditorObjectFlags.BBOX | EditorObjectFlags.MAPMARKER | EditorObjectFlags.OBJECTMARKER | EditorObjectFlags.LISTITEM;
 const int EFE_CAMERA_DEFAULT = EFE_DEFAULT | EditorObjectFlags.EDITOR_ONLY;
 const int EFE_ATTACHMENT = 0;
-const int EFE_BRUSHED = EditorObjectFlags.LISTITEM | 0;
+const int EFE_BRUSHED = EditorObjectFlags.LISTITEM | EditorObjectFlags.BRUSHED | 0;
 enum EditorObjectFlags
 {
 	NONE = 0,
@@ -17,4 +17,5 @@ enum EditorObjectFlags
 	HIDDEN = 1024,
 	NOSAVE = 2048,
 	NODELETE = 4096,
+	BRUSHED = 8192,
 };
