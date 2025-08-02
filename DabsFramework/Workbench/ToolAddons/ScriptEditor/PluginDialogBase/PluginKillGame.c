e3b3c890fdf133abed5b7f50407ec6c500dd4d3a
Tyler Paul <tylerpaul2329@gmail.com>
2025-08-02 04:52:47 -0400

    Speed up kill game

--- DabsFramework/Workbench/ToolAddons/ScriptEditor/PluginDialogBase/PluginKillGame.c
+++ DabsFramework/Workbench/ToolAddons/ScriptEditor/PluginDialogBase/PluginKillGame.c
@@ -1,8 +1,15 @@
 [WorkbenchPluginAttribute("Close Game", "Kills all running instances of DayZ", "F10", "", {"ScriptEditor", "ResourceManager"})]
-class PluginKillGame: PluginProject
+class PluginKillGame: PluginDialogBase
 {
 	void PluginKillGame()
 	{
-		KillTask(m_LaunchSettings.GetExecutableName());
+		string current_dir = GetCurrentDirectory();
+		LaunchSettings launch_settings = LaunchSettings.Load(current_dir + "launch.bin");
+		if (!launch_settings) {
+			ErrorDialog(string.Format("Failed to load %1 in %2", "launch.bin", current_dir));
+			return;
+		}
+		
+		Workbench.RunCmd(string.Format("taskkill /f /im %1 /t", launch_settings.GetExecutableName()));
 	}
 }
