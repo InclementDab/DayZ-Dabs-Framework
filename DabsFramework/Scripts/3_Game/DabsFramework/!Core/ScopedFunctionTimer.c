7504bcb881d830b0d2fc6ae6f2e7a5653af14abe
Tyler Paul <tylerpaul2329@gmail.com>
2025-09-18 06:38:06 -0400

    Adding ticks to the function timer

--- DabsFramework/Scripts/3_Game/DabsFramework/!Core/ScopedFunctionTimer.c
+++ DabsFramework/Scripts/3_Game/DabsFramework/!Core/ScopedFunctionTimer.c
@@ -34,14 +34,23 @@ class ScopedFunctionTimer: Managed
 	private static ref map<string, int> s_TotalFunctionTime = new map<string, int>();
 	
 	protected int m_StartTime = -1, m_LastDumpTime = -1;
+	protected int m_StartTick;
 	protected string m_FunctionName;
 	protected int m_ActionIncrement;
 	
-	void ScopedFunctionTimer(string function_name)
+	void ScopedFunctionTimer(string function_name = string.Empty)
 	{
 #ifdef DEBUG_SCOPED_FUNCTION_TIMERS
+		if (function_name == string.Empty) {
+			string stack;
+			DumpStackString(stack);
+			
+			Print(stack);
+		}
+		
 		m_FunctionName = function_name;
 		m_StartTime = g_Game.GetTime();
+		m_StartTick = TickCount(0);
 		m_LastDumpTime = m_StartTime;
 #endif
 	}	
@@ -50,16 +59,13 @@ class ScopedFunctionTimer: Managed
 	{
 #ifdef DEBUG_SCOPED_FUNCTION_TIMERS
 		string average_time_per_action;
+		int tick_total = TickCount(m_StartTick);
 		if (m_ActionIncrement) {
-			float denominator = 1;
-			if (g_Game.GetTime() != m_LastDumpTime) {
-				denominator = g_Game.GetTime() - m_LastDumpTime;
-			}
-			average_time_per_action = string.Format(" (Average Time Per Action: [%1ms])", m_ActionIncrement / denominator);
+			average_time_per_action = string.Format(" (Average Time Per Action: [%1ms, %2 ticks each])", (g_Game.GetTime() - m_LastDumpTime) / m_ActionIncrement, tick_total / m_ActionIncrement);
 		}
 		
 		int delta_time = g_Game.GetTime() - m_StartTime;
-		PrintFormat("%1: %2ms%3", m_FunctionName, delta_time, average_time_per_action);
+		PrintFormat("%1: %2ms (%4 total ticks)%3", m_FunctionName, delta_time, average_time_per_action, tick_total);
 		
 		if (!s_TotalFunctionTime) {
 			s_TotalFunctionTime = new map<string, int>();
