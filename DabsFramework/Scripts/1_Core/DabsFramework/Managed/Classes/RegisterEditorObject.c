daaa4807dfc5e861510e0450ad9447de00846158
Tyler Paul <tylerpaul2329@gmail.com>
2025-12-05 03:52:39 -0500

    Update RegisterEditorObject.c

--- DabsFramework/Scripts/1_Core/DabsFramework/Managed/Classes/RegisterEditorObject.c
+++ DabsFramework/Scripts/1_Core/DabsFramework/Managed/Classes/RegisterEditorObject.c
@@ -2,7 +2,7 @@ class RegisterScriptedEntity: Class
 {
 	static ref array<ref Param3<typename, string, string>> Instances = {};
 	
-	void RegisterScriptedEntity(typename type, string display_name, string icon)
+	void RegisterScriptedEntity(typename type, string display_name, string icon = "set:solid image:circle")
 	{		
 		Instances.Insert(new Param3<typename, string, string>(type, display_name, icon));
 	}
